ba12b6cb7bfb9c2c27ef3fd0befa410c2699ddd7
Sonmi Kos <sonmi.kos@gmail.com>
2019-10-03 21:43:34 -0500

    added react build

---
+++ client/build/precache-manifest.6518590f45852080bf1c57fa27b12aa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "187453c01f0b4a02e4b0",
+    "url": "/static/js/main.bb0b9617.chunk.js"
+  },
+  {
+    "revision": "8589cfe95446d3fd0a4e",
+    "url": "/static/js/2.1d07b520.chunk.js"
+  },
+  {
+    "revision": "187453c01f0b4a02e4b0",
+    "url": "/static/css/main.b1f96571.chunk.css"
+  },
+  {
+    "revision": "2980e4678c03ce146fd014a738467839",
+    "url": "/index.html"
+  }
+];
